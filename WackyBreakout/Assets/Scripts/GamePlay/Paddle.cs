34fc7ca9fa26670e0022c7dd0082922afc7dccba
Vũ Đức Trung <vuductrung14112001@gmail.com>
2022-12-26 02:23:38 +0700

    Keep the Paddle in the Playfield

--- WackyBreakout/Assets/Scripts/GamePlay/Paddle.cs
+++ WackyBreakout/Assets/Scripts/GamePlay/Paddle.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
     Rigidbody2D _rb;
+    float _halfWidth;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = this.GetComponent<Rigidbody2D>();
+        _halfWidth = this.GetComponent<BoxCollider2D>().size.x / 2;
     }
 
     // Update is called once per frame
@@ -21,7 +23,30 @@ public class Paddle : MonoBehaviour
     private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
+        
+        if (horizontal != 0)
+        {
+            float newPosX = this.transform.position.x + horizontal * Time.deltaTime * ConfigurationUtils.PaddleMoveUnitsPerSecond;
+            newPosX = CalculateClampedX(newPosX);
+            this._rb.MovePosition(new Vector3(newPosX, this.transform.position.y, 0));
+        }
+    }
 
-        this._rb.MovePosition(this.transform.position + new Vector3(horizontal * Time.deltaTime * ConfigurationUtils.PaddleMoveUnitsPerSecond, 0));
+    float CalculateClampedX(float possibleNewX)
+    {
+        float boundLeft = ScreenUtils.ScreenLeft + _halfWidth;
+        float boundRight = ScreenUtils.ScreenRight - _halfWidth;
+
+        if (possibleNewX < boundLeft)
+        {
+            return boundLeft;
+        }
+
+        if (possibleNewX > boundRight)
+        {
+            return boundRight;
+        }
+
+        return possibleNewX;
     }
 }
